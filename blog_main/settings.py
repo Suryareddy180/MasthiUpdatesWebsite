226eec022dd5fa369eb572ba261a1827518243f3
Suryareddy180 <suryareddynallimilli@gmail.com>
2026-05-11 16:25:31 +0530

    Remove hardcoded secrets for security

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-rbaehy#t=_om9(sh%5w+#l!ht5-)n(79=w^u8n=jy75@j3w941')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Set to False when deploying to production
@@ -174,8 +174,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'suryareddynallimilli@gmail.com')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'vgln wwpr oxjk clcy')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = 'Masthi Updates <noreply@masthiupdates.com>'
 
 # Login/Logout Redirect URLs
